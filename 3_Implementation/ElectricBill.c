da61c0cc1b37691c28a8c77b2c124dd3106dca5d
Nagendramalakalapalli <nagendramalakalapalli@gmail.com>
2022-02-06 12:27:04 +0530

    Update README,#1

--- 3_Implementation/ElectricBill.c
+++ 3_Implementation/ElectricBill.c
@@ -1,6 +1,6 @@
 /**
  * @file ElectricBill.c
- * @author your name Malakalapalli Nagendra
+ * @author Malakalapalli Nagendra
  * @brief Electricity Bill Calculator
  * @version 0.1
  * @date 2022-02-05
@@ -9,9 +9,137 @@
  * 
  */
 
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
 #include<string.h>
-#include<Windows.h>
 
+
+/
+struct ElectricBill
+{
+	char Location[100];
+	char Biller_Name[30];
+	int Service_Number;
+    float Units_Consumed;
+    int Mobile_Number[10];
+	char Biller_Mail[100];
+	char Biller_Address[100];
+	
+	
+};
+struct ElectricBill E;
+
+/**
+ * @brief Information of Biller
+ * 
+ */
+
+void info()
+{
+    printf("Enter your  Location :\n");
+	scanf("%s", E.Location);
+	printf("Enter your  Biller Name:\n");
+	scanf("%s", E.Biller_Name);
+	printf("Enter the Mobile Number : \n");
+    scanf("%s", E.Mobile_Number);
+    printf("Enter Biller Mail ID :\n");
+    scanf("%s", E.Biller_Mail);
+    printf("Enter the permanent address :\n");
+    scanf("%s", E.Biller_Address);
+    printf("Enter your Service Number:\n");
+	scanf("%d", &E.Service_Number);
+    printf("Enter Units Consumed:\n");
+	scanf("%f", &E.Units_Consumed);
+	
+    
+}
+
+void Urban()
+{
+    float amount=0;
+    E.Units_Consumed;
+    if(E.Units_Consumed <=30 && E.Units_Consumed >= 0)
+    {
+        amount = E.Units_Consumed * 3.25; //cost per unit 3.25 is rupees
+    }
+    else if(E.Units_Consumed >= 31 && E.Units_Consumed <= 100)
+    {
+        amount = E.Units_Consumed * 4.70;
+    }
+    else if(E.Units_Consumed >= 101 && E.Units_Consumed <= 200)
+    {
+        amount = E.Units_Consumed * 6.25;
+    }
+    else
+    {
+        amount = E.Units_Consumed * 7.30;
+    }
+
+    printf(" \t\t\t\t\t\t\t\t\t ****** Domestic Electricity Bill****** \t\t\t\t\t \n\n");
+    printf("Name : %s\n", E.Biller_Name);
+    printf("Service Number : %d\n",E.Service_Number );
+    printf("In Urban, your electricity bill is: %f\n", amount);
+    printf("Units you consumed per month: %d\n", E.Units_Consumed);
+}
+
+void Rural()
+{
+    float amount=0;
+    E.Units_Consumed;
+    if(E.Units_Consumed <=30 && E.Units_Consumed >= 0)
+    {
+        amount = E.Units_Consumed * 3.15; //3.15 is rupees
+    }
+    else if(E.Units_Consumed >= 31 && E.Units_Consumed <= 100)
+    {
+        amount = E.Units_Consumed * 4.40;
+    }
+    else if(E.Units_Consumed >= 101 && E.Units_Consumed <= 200)
+    {
+        amount = E.Units_Consumed * 5.95;
+    }
+    else
+    {
+        amount = E.Units_Consumed * 6.80;
+    }
+   printf(" \t\t\t\t\t\t\t ****** Domestic Electricity Bill****** \t\t\t\t\t \n\n");
+    printf("Name : %s\n", E.Biller_Name);
+    printf("Service Number : %d\n",E.Service_Number );
+    printf("In Rural, your electricity bill is: %f\n", amount);
+    printf("Units you consumed per month: %d\n", E.Units_Consumed);
+ 
+}
+
+int main()
+{ 
+	info();
+	int option;
+	do
+	{
+		printf(" \t\t\t\t\t\t  ****** Domestic Electricity Bill****** \t\t\t\t\t\t\t \n");
+		printf("Select enter your choice (1-2):\n");
+		printf("1. Urban\n");
+		printf("2. Rural\n");
+		printf("3. EXIT\n");
+		printf("Electricity Board Helpline: 8435 2340\n");
+		printf("Enter your choice :\n");
+		scanf("%d", &option);
+		system("cls");
+		switch (option)
+		{
+		case 1:
+			Urban();
+			break;
+		case 2:
+			Rural();
+			break;
+		default:
+			printf("SORRY INVALID CHOICE!\n");
+			printf("PLEASE CHOOSE FROM 1 or 2\n");
+		}
+		getch();
+	} while (option != 3);
+}
+  
